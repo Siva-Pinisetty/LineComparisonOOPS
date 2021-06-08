03ec915d716e0cfebf801a82beb68bb3c3b846e3
balu-pinisetty <balupinisetty@gmail.com>
2021-06-09 05:13:44 +0530

    Added: Object method for Dispalying Welcome message

---
+++ LineComparison.java
@@ -0,0 +1,16 @@
+package com.bridzlab.linecomparisonoops;
+
+public class LineComparison {
+	
+	public static void main(String[] args) {
+		LineComparison lineComparisonObj = new LineComparison();
+		lineComparisonObj.welcomeDisplay();
+	}
+	
+	/**
+	 * Displaying Welcome Message.
+	 */
+	public void welcomeDisplay() {
+		System.out.println("Welcome to Line Comparison Computation Program\n");
+	}
+}
