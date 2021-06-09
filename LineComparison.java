fca3c4eae69212ad392ca33995a32999d3c2cdaa
balu-pinisetty <balupinisetty@gmail.com>
2021-06-09 05:57:42 +0530

    Refactor: Code to Object method for  calculating two line lengths and checks for comparison

--- LineComparison.java
+++ LineComparison.java
@@ -7,7 +7,11 @@ public class LineComparison {
 	public static void main(String[] args) {
 		LineComparison lineComparisonObj = new LineComparison();
 		lineComparisonObj.welcomeDisplay();
-		lineComparisonObj.lengthOfLine();
+		LineComparison Line1Obj = new LineComparison();
+		float length1= lineComparisonObj.lengthOfLine("Line1",3,4,2,6);
+		LineComparison Line2Obj = new LineComparison();
+		float length2= lineComparisonObj.lengthOfLine("Line2",9,5,3,7);
+		lineComparisonObj.lineComparison(length1, length2);
 	}
 	
 	/**
@@ -19,30 +23,16 @@ public class LineComparison {
 	
 	/**
 	 * Calculating length of two lines using the Cartesian formula and comparing
+	 * @return 
 	 */
-	private void lengthOfLine()
-	{
-		
-		System.out.println("enter the x1 value for Line1:");
-		int x1=scanner.nextInt();
-		System.out.println("enter the y1 value for Line1:");
-		int y1=scanner.nextInt();
-		System.out.println("enter the x2 value for Line1:");
-		int x2=scanner.nextInt();
-		System.out.println("enter the y2 value for Line1:");
-		int y2=scanner.nextInt();
-		System.out.println("enter the a1 value for Line2:");
-		int a1=scanner.nextInt();
-		System.out.println("enter the b1 value for Line2:");
-		int b1=scanner.nextInt();
-		System.out.println("enter the a2 value for Line2:");
-		int a2=scanner.nextInt();
-		System.out.println("enter the b2 value for Line2:");
-		int b2=scanner.nextInt();
-		float length1=(float) Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
-		System.out.println("Length of LINE1 b/n points "+"("+x1+","+y1+") & "+"("+x2+","+y2+") "+"is "+length1);
-		float length2=(float) Math.sqrt(Math.pow(a2-a1,2)+ Math.pow(b2-b1,2));
-		System.out.println("Length of LINE2 b/n points "+"("+a1+","+b1+") & "+"("+a2+","+b2+") "+"is "+length2);
+	public float lengthOfLine(String LINE, int x1, int x2, int y1, int y2) {
+		float length=(float) Math.sqrt(Math.pow(x2-x1,2)+ Math.pow(y2-y1,2));
+		System.out.println("Length of "+LINE+" b/n points "+"("+x1+","+y1+") & "+"("+x2+","+y2+") "+"is "+length);
+		return length;
+	}
+	
+	
+	public void lineComparison(float length1, float length2) {
 		int val = Float.compare(length1, length2);
 	    //System.out.println(val);
 		if(val == 0)
